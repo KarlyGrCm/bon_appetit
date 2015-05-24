4c02b315ff16a7024ede846e7a6904a2a72ced29
Karla Garcia <karla.garcia.campos@gmail.com>
2015-05-24 13:22:35 -0500

    create database migration and update schema

---
+++ db/migrate/20150524170946_create_restaurants.rb
@@ -0,0 +1,14 @@
+class CreateRestaurants < ActiveRecord::Migration
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.string :phone
+      t.string :rfc
+      t.text :description
+      t.integer :category_id, index: true
+
+      t.timestamps null: false
+    end
+  end
+end
