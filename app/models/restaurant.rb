0f4950493d4c351bb2272a6906f87c49112b7de6
Karla Garcia <karla.garcia.campos@gmail.com>
2015-05-24 15:10:13 -0500

    modify restaurant and add relationship with dishes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ActiveRecord::Base
 	belongs_to :category
+	has_many :dishes
 	validates_presence_of :name, :phone, :address, :rfc, :description, :category_id, message: 'El campo es requerido'
 end
