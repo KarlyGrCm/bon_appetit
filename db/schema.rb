8ba02622e989462b4896e028ccb71abc5a0fbb0a
Karla Garcia <karla.garcia.campos@gmail.com>
2015-05-24 14:01:14 -0500

    schema modified by migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150524170946) do
+ActiveRecord::Schema.define(version: 20150524183444) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
