52a7208c1deea8f7125b4c7467bfa65b3aaadea6
Karla Garcia <karla.garcia.campos@gmail.com>
2015-05-24 18:26:47 -0500

    modify models to add images in database

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,4 +1,5 @@
 class Dish < ActiveRecord::Base
 	belongs_to :restaurant
-	validates_presence_of :name, :price, :description , message: 'Este campo es requerido'
+	validates_presence_of :name, :price, :description, :image, message: 'Este campo es requerido'
+	mount_uploader :image, ImageUploader
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ActiveRecord::Base
 	belongs_to :category
 	has_many :dishes
-	validates_presence_of :name, :phone, :address, :rfc, :description, :category_id, message: 'El campo es requerido'
+	validates_presence_of :name, :phone, :address, :rfc, :description, :image, :category_id, message: 'El campo es requerido'
+	mount_uploader :image, ImageUploader
 end
