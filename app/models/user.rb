c72a9715549b7e632692b0ebb16e16b28af09005
Karla Garcia <karla.garcia.campos@gmail.com>
2015-05-24 22:09:50 -0500

    models to roles and relationships between role and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,26 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  belongs_to :role
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  after_create :assing_role
+
+  def is_admin?
+    self.role.name == 'Administrador'
+  end
+
+  def is_client? 
+    self.role.name == 'Cliente'
+  end
+
+  def is_restaurant?
+    self.role.name == 'Restaurantero'
+  end
+
+  private
+  def assing_role
+  	self.role = Role.find_by(name: 'Cliente')
+  	self.save
+  end
 end
