1f2b819d9d34477ebb3cd0b1b5705c9fe465915c
Karla Garcia <karla.garcia.campos@gmail.com>
2015-05-24 22:10:36 -0500

    update controller to manage roles and sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+  def after_sign_in_path_for(user)
+  	if user.is_admin?
+  		restaurants_path
+  	elsif user.is_client?
+  		root_url
+  	end
+  end
 end
